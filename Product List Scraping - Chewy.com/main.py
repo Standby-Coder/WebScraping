c434f23f4ff3070dd0ced363612937c17bd37ca7
Standby-Coder <ckreddy573@gmail.com>
2023-09-03 22:11:20 +0530

    Initial commit

---
+++ Product List Scraping - Chewy.com/main.py
@@ -0,0 +1,157 @@
+import os
+import requests
+import json
+import bs4
+import re
+import pandas as pd
+import html5lib
+import warnings
+warnings.filterwarnings("ignore")
+
+from tqdm import tqdm
+from util.get_product_details import *
+from util.spider import *
+
+# # print everything to a file
+# import sys
+# sys.stdout = open("output.txt", "w")
+
+def get_product_info(url):
+    headers = ({'User-Agent':
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 OPR/101.0.0.0',
+            'Accept-Language': 'en-US, en;q=0.5'})
+    response = requests.get(url, headers = headers)
+    soup = bs4.BeautifulSoup(response.text, "html.parser")
+    spec_info = get_spec(soup)
+
+    product_info = {}
+    
+    product_info["url"] = url
+    product_info["title"] = get_title(soup)
+    product_info["brand"] = get_brand(soup)
+    product_info["advertised_price"] = get_advertised_price(soup)
+    product_info["autoship_price"] = get_autoship_price(soup)
+    product_info["weight"] = spec_info["weight"]
+    product_info["desc"] = spec_info["desc"]
+    product_info["item_number"] = spec_info["item_number"]
+    product_info["Prescription"] = spec_info["Prescription"]
+
+    return product_info
+
+def print_product_info(prod_info):
+    print("URL: " + prod_info["url"])
+    print("Title: " + prod_info["title"])
+    print("Brand: " + prod_info["brand"])
+    print("Advertised Price: " + prod_info["advertised_price"])
+    print("Autoship Price: " + prod_info["autoship_price"])
+    print("Weight: " + prod_info["weight"])
+    print("Description: " + prod_info["desc"])
+    print("Item Number: " + prod_info["item_number"])
+    print("Prescription: " + prod_info["Prescription"])
+
+def main(url, df1 = None):
+    # url = ["https://www.chewy.com/frisco-steel-framed-elevated-dog-bed/dp/139415",
+    #         "https://www.chewy.com/american-journey-minced-salmon-tuna/dp/160945",
+    #         "https://www.chewy.com/simparica-trio-chewable-tablet-dogs/dp/251350",
+    #         "https://www.chewy.com/carprofen-generic-caplets-dogs/dp/173410"]
+
+    df = pd.DataFrame(columns = ["item_number", "url", "title", "brand", "advertised_price", "autoship_price", "weight", "desc", "Prescription"])
+
+    next_level = url
+
+    if df1 is None:
+        i = 0
+    else:
+        i = len(os.listdir("./prodlist"))
+
+    product_url_list = []
+
+    try:
+        for next_url in next_level:
+            if next_url.startswith("https://www.chewy.com"):
+                product_url_list, next_level_list = spider(next_url, [])
+                print("URL = " , next_url)
+            else:
+                product_url_list, next_level_list = spider("https://www.chewy.com" + next_url, [])
+                print("URL = " , "https://www.chewy.com" + next_url)
+            
+            print("Product List Length = " , len(product_url_list))
+            print("Next Length = " , len(next_level_list))
+            
+            for j in tqdm(product_url_list):
+                if j.startswith("https://www.chewy.com"):
+                    prod_info = get_product_info(j)
+                else:
+                    prod_info = get_product_info("https://www.chewy.com" + j)
+                if prod_info["item_number"] == "":
+                    continue
+
+                if df1 is not None:
+                    if prod_info["item_number"] in df1["item_number"].values:
+                        continue
+                
+                if prod_info["item_number"] in df["item_number"].values:
+                    continue
+
+                df = df.append(prod_info, ignore_index = True)
+            
+            for j in next_level_list:
+                if j not in next_level:
+                    next_level.append(j)
+
+            print("Next Level Length = " , len(next_level))
+
+            if len(df) > 100:
+                df.to_csv(f"./prodlist/product_info_{i}.csv", index = False)
+                df = pd.DataFrame(columns = ["item_number", "url", "title", "brand", "advertised_price", "autoship_price", "weight", "desc", "Prescription"])
+                i = i + 1
+    except Exception as e:
+        print("Unexpected error, dumping current df......")
+        df.to_csv(f"./prodlist/product_info_{i}.csv", index = False)
+        e.print_stack()
+
+    df.to_csv(f"./prodlist/product_info_{i}.csv", index = False)
+        
+
+def debug(url):
+    headers = ({'User-Agent':
+            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+            'Accept-Language': 'en-US, en;q=0.5'})
+    response = requests.get(url, headers = headers)
+    soup = bs4.BeautifulSoup(response.text, "html.parser")
+    # info = soup.find("div", attrs={"data-event-label" : "product-detail-description"})
+    # head = info.find(lambda tag:tag.name == "h3" and "Details" in tag.text)
+    # div = head.find_next("div")
+    # desc = div.find("p")
+    for a in soup.find_all('a', href=True):
+        if re.search("/dp/", a['href']):
+            print(a['href'])
+
+def combine_df(mode = "save"):
+    df = pd.DataFrame(columns = ["item_number", "url", "title", "brand", "advertised_price", "autoship_price", "weight", "desc", "Prescription"])
+    
+    # list of csv files named as product_info
+    csv_list = os.listdir("./prodlist")
+    csv_list = [i for i in csv_list if i.startswith("product_info")]
+    csv_list.sort()
+
+    for csv in csv_list:
+        df = df.append(pd.read_csv(f"./prodlist/{csv}"), ignore_index = True)
+
+    if mode == "save":
+        df.to_csv("./prodlist/product_info.csv", index = False)
+    elif mode == "return":
+        return df
+    
+if __name__ == "__main__":
+    start_url = ["/"]
+
+    df = None
+    if os.path.exists("./prodlist"):
+        df = combine_df(mode = "return")
+
+    main(start_url, df)
+    # debug("https://www.chewy.com/b/birthday-shop-2700")
+    # debug("https://www.chewy.com/simparica-trio-chewable-tablet-dogs/dp/251350")
+    # debug("https://www.chewy.com/american-journey-minced-salmon-tuna/dp/160945")
+
---
+++ Product List Scraping - Chewy.com/util/spider.py
@@ -0,0 +1,69 @@
+import requests
+import bs4
+import re
+
+# def get_product_urls(soup):
+#     product_url_list = []
+#     for a in soup.find_all('a', href=True):
+#         if re.search("https://www.chewy.com/", a['href']):
+#             product_url_list.append(a['href'])
+    
+#     product_url_list = list(set(product_url_list))
+
+#     # seperate those which contains "/dp/[0-9]*" and those which don't
+#     product_url_list_1 = []
+#     product_url_list_2 = []
+#     for url in product_url_list:
+#         if re.search("/dp/[0-9]*", url):
+#             product_url_list_1.append(url)
+#         else:
+#             product_url_list_2.append(url)
+
+#     return product_url_list_1, product_url_list_2
+
+# def spider(url):
+#     headers = ({'User-Agent':
+#             'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+#             'Accept-Language': 'en-US, en;q=0.5'})
+#     response = requests.get(url, headers = headers)
+#     soup = bs4.BeautifulSoup(response.text, "html.parser")
+    
+#     product_url_list, next_level_list = get_product_urls(soup)
+
+#     return product_url_list, next_level_list
+
+# create a web spider which can crawl through the website and get all the product urls which are listed on the website
+
+def get_product_urls(soup):
+    product_url_list = []
+    next_level_list = []
+    for a in soup.find_all('a', href=True):
+        if re.search("/dp/", a['href']):
+            if a['href'].startswith("https://www.chewy.com") or a['href'].startswith("/"):
+                product_url_list.append(a['href'])
+        elif re.search("/b/", a['href']) or re.search("/brands/", a['href']):
+            next_level_list.append(a['href'])
+
+    product_url_list = list(set(product_url_list))
+    next_level_list = list(set(next_level_list))
+
+    return product_url_list, next_level_list
+
+def spider(url, next_level_list):
+    headers = ({'User-Agent':
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 OPR/101.0.0.0',
+            'Accept-Language': 'en-US, en;q=0.5'})
+    
+    product_url_list_new, next_level_list_new = [] , []
+
+    for i in url:
+        response = requests.get(url, headers = headers)
+        soup = bs4.BeautifulSoup(response.text, "html.parser")
+
+        product_url_list_new, next_level_list_new = get_product_urls(soup)
+        
+
+    product_url_list_new = list(set(product_url_list_new))
+    next_level_list_new = list(set(next_level_list_new))
+
+    return product_url_list_new, next_level_list_new
